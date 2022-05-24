19a0d35d92a82081afa542cde3acbb0dd7b496ae
byoo <blazeryoo1921@gmail.com>
2022-05-24 04:00:42 +0000

    Move index.html

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
-app.use(express.static(path.join(__dirname + '/files')));
+app.use(express.static(path.join(__dirname + '/')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/files/index.html'));
+  res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.get('*', function (req, res) {
